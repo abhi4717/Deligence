126a720b774adda3adfb001cf453c9220191b121
Abhishek Gopalan <abhi4717@gmail.com>
2018-03-29 17:16:34 +0530

    Added initial project files

---
+++ src/Deligence/App_Start/RouteConfig.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace Deligence
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(name: "TASK", url: "Task", defaults: new { controller = "Task", action = "Index" });
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Task", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ src/Deligence/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Deligence.Global" Language="C#" %>
